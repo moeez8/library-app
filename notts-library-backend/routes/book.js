2160275c4ab5215f21998ba38601a9b3f7633991
moeez.shahid <moeez.shahid@bjss.com>
2022-05-31 11:53:26 +0100

    updated search function to serach by title

--- notts-library-backend/routes/book.js
+++ notts-library-backend/routes/book.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database")
 const Book = require("../models/Book");
+const Sequelize = require("sequelize")
+const Op = Sequelize.Op;
 
 //Get All Books
 router.get("/", (req, res) => {
@@ -15,41 +18,49 @@ router.get("/", (req, res) => {
     });
 });
 
-//Get Book By Id
-router.get("/:id", (req, res) => {
-  if (req.params.id) {
-    console.log(req.params.id);
-    res.sendStatus(200);
-  }
-  res.sendStatus(405);
-});
-
 //Search For Book
-router.get("/search", (req, res) => {
+router.get('/search', (req, res) => {
   const { term } = req.query;
-  console.log(term);
-  res.sendStatus(200);
-});
 
-//Add New Book
-router.post("/add", (req, res) => {
-  Book.create({
-    title: "bestbook",
-  })
-    .then(() => res.sendStatus(200))
+  Book.findAll({ where: { title: { [Op.like]: '%' + term + '%' } } })
+    .then((books) => {
+      console.log(books);
+      res.send(books);
+    })
     .catch((err) => {
-      console.log(err);
+      console.log("Error: " + err);
       res.sendStatus(400);
     });
-});
 
-//Update A Book
-router.put("/:id", (req, res) => {
-  if (req.params.id) {
-    console.log(req.params.id);
-    res.sendStatus(200);
-  }
-  res.sendStatus(405);
-});
+  //Add New Book
+  router.post("/add", (req, res) => {
+    Book.create({
+      title: "bestbook",
+    })
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  });
 
-module.exports = router;
+  //Update A Book
+  router.put("/:id", (req, res) => {
+    if (req.params.id) {
+      console.log(req.params.id);
+      res.sendStatus(200);
+    }
+    res.sendStatus(405);
+  });
+
+  //Get Book By Id
+  router.get("/:id", (req, res) => {
+    if (req.params.id) {
+      console.log(req.params.id);
+      res.sendStatus(200);
+    }
+    res.sendStatus(405);
+  });
+
+
+  module.exports = router;
