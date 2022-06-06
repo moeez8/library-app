c590235cfb442bf65db26d943ff1a362454c65c0
george.hanks <george.hanks@bjss.com>
2022-06-06 10:41:31 +0100

    Started Work On Landing Page

--- notts-library-frontend/src/App.tsx
+++ notts-library-frontend/src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <div className="max-w-7xl m-auto">
+      <header className="bg-white flex justify-between rounded-md shadow-md p-4 m-2">
+        <div className="flex flex-col justify-around ">
+          <h1 className="text-xl font-bold">Dashboard</h1>
+        </div>
+        <div className="flex flex-col justify-around ">
+          <div className="flex gap-4">
+            <h2 className="rounded-sm shadow-md p-2">Home</h2>
+            <h2 className="rounded-sm shadow-md p-2">Books</h2>
+            <h2 className="rounded-sm shadow-md p-2">Add A Book</h2>
+            <h2 className="rounded-sm shadow-md p-2">Request A Book</h2>
+          </div>
+        </div>
+      </header>
+      <div className="bg-white rounded-md shadow-md p-4 m-2"></div>
+    </div>
+  );
 }
 
 export default App;
