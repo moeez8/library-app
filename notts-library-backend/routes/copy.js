8b50773c8f24ceaf1ffac346d048ea2706371c2f
moeez.shahid <moeez.shahid@bjss.com>
2022-06-09 15:49:04 +0100

    Added purchase request API and updated route

--- notts-library-backend/routes/copy.js
+++ notts-library-backend/routes/copy.js
@@ -2,22 +2,14 @@ const express = require("express");
 const router = express.Router();
 const copyAPI = require("../api/copy-api")
 
-//Get All Copies
 router.get("/", copyAPI().getAllCopies);
-
-//Add New Copy
 router.post("/add", copyAPI().addNewCopy);
-
-//Get A Copy By ID
 router.get("/:id", copyAPI().getCopyByID)
 
 //Get Withdraws Of A Copy By ID
 router.get("/:id/withdraws", copyAPI().getCopyWithdrawsByID)
 
-//Check In A Copy By ID
 router.get("/:id/check-in", copyAPI().checkinCopyByID)
-
-//Check Out A Copy By ID
 router.get("/:id/check-out", copyAPI().checkoutCopyByID)
 
 //Check Copy Status
