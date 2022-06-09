ae64282dd29785fa44e863b1d7a6aa77eecba53d
George Hanks <george.hanks@bjss.com>
2022-06-09 15:51:29 +0100

    Cleaned Up Formatting Of Route Files

--- notts-library-backend/routes/copy.js
+++ notts-library-backend/routes/copy.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const copyAPI = require("../api/copy-api")
+const copyAPI = require("../api/copy-api");
 
 router.get("/", copyAPI().getAllCopies);
 router.post("/add", copyAPI().addNewCopy);
-router.get("/:id", copyAPI().getCopyByID)
-
-//Get Withdraws Of A Copy By ID
-router.get("/:id/withdraws", copyAPI().getCopyWithdrawsByID)
-
-router.get("/:id/check-in", copyAPI().checkinCopyByID)
-router.get("/:id/check-out", copyAPI().checkoutCopyByID)
-
-//Check Copy Status
-router.get("/:id/status", copyAPI().checkCopyStatus)
+router.get("/:id", copyAPI().getCopyByID);
+router.get("/:id/withdraws", copyAPI().getCopyWithdrawsByID);
+router.get("/:id/check-in", copyAPI().checkinCopyByID);
+router.get("/:id/check-out", copyAPI().checkoutCopyByID);
+router.get("/:id/status", copyAPI().checkCopyStatus);
 
 module.exports = router;
