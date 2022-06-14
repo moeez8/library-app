e2a1e5e44ed46e305f9690638f4d7fcd4bc4de84
moeez.shahid <moeez.shahid@bjss.com>
2022-06-14 13:54:17 +0100

    worked on copy service

--- notts-library-backend/src/api/copy-api.ts
+++ notts-library-backend/src/api/copy-api.ts
@@ -25,139 +25,151 @@ const copyApi = () => {
 	};
 
 	const getCopyByID = async (req: Request, res: Response) => {
-		if (req.params && req.params.id && typeof req.params.id === "string") {
-			let id = parseInt(req.params.id);
-			res.json(await CopyService().GetCopyByID(id));
-			return;
-		}
+		let id = parseInt(req.params.id);
+		res.json(await CopyService().GetCopyByID(id));
+		return;
 	};
 
 	const getCopyWithdrawsByID = async (req: Request, res: Response) => {
-		if (req.params && req.params.id && typeof req.params.id === "string") {
-			let id = parseInt(req.params.id);
-			res.json(await CopyService().GetCopyWithdrawsByID(id));
-			return;
-		}
+		let id = parseInt(req.params.id);
+		res.json(await CopyService().GetCopyWithdrawsByID(id));
+		return;
 	};
 
-	const checkinCopyByID = (req: Request, res: Response) => {
-		models.copy
-			.findByPk(parseInt(req.params.id), {
-				include: [{ model: models.withdraw, as: "withdraws" }],
-			})
-			.then((row: any) => {
-				if (row) {
-					//Can Copy Be Checked Out?
+	const checkinCopyByID = async (req: Request, res: Response) => {
 
-					const array = [...row.withdraws];
+		let id = parseInt(req.params.id);
+		res.json(await CopyService().CheckinCopyByID(id));
+		return;
 
-					array.sort((a, b) => {
-						if (a.date_out < b.date_out) return 1;
-						return -1;
-					});
+		// models.copy
+		// 	.findByPk(parseInt(req.params.id), {
+		// 		include: [{ model: models.withdraw, as: "withdraws" }],
+		// 	})
+		// 	.then((row: any) => {
+		// 		if (row) {
+		// 			//Can Copy Be Checked Out?
 
-					if (array[0] && !array[0].date_in) {
-						models.withdraw.findByPk(array[0].id).then((rowb: any) => {
-							rowb.update({ date_in: new Date() });
-							res.send(rowb);
-						});
-					} else {
-						res.status(400).send("Copy Could Not Be Checked In");
-					}
-				} else {
-					res.status(400).json({ error: "Failed To Find Row" });
-				}
-			})
-			.catch((err: any) => {
-				res.status(400).json({ error: "Failed To Find Row", msg: err });
-			});
+		// 			const array = [...row.withdraws];
+
+		// 			array.sort((a, b) => {
+		// 				if (a.date_out < b.date_out) return 1;
+		// 				return -1;
+		// 			});
+
+		// 			if (array[0] && !array[0].date_in) {
+		// 				models.withdraw.findByPk(array[0].id).then((rowb: any) => {
+		// 					rowb.update({ date_in: new Date() });
+		// 					res.send(rowb);
+		// 				});
+		// 			} else {
+		// 				res.status(400).send("Copy Could Not Be Checked In");
+		// 			}
+		// 		} else {
+		// 			res.status(400).json({ error: "Failed To Find Row" });
+		// 		}
+		// 	})
+		// 	.catch((err: any) => {
+		// 		res.status(400).json({ error: "Failed To Find Row", msg: err });
+		// 	});
 	};
 
-	const checkoutCopyByID = (req: Request, res: Response) => {
-		models.copy
-			.findByPk(parseInt(req.params.id), {
-				include: [{ model: models.withdraw, as: "withdraws" }],
-			})
-			.then((row: any) => {
-				if (row) {
-					//Can Copy Be Checked Out?
+	const checkoutCopyByID = async (req: Request, res: Response) => {
 
-					const array = [...row.withdraws];
+		let id = parseInt(req.params.id);
+		res.json(await CopyService().CheckoutCopyByID(id));
+		return;
 
-					array.sort((a, b) => {
-						if (a.date_out < b.date_out) return 1;
-						return -1;
-					});
+		// models.copy
+		// 	.findByPk(parseInt(req.params.id), {
+		// 		include: [{ model: models.withdraw, as: "withdraws" }],
+		// 	})
+		// 	.then((row: any) => {
+		// 		if (row) {
+		// 			//Can Copy Be Checked Out?
 
-					if (array[0]) {
-						if (array[0].date_in != null) {
-							models.withdraw
-								.create({
-									copy_id: req.params.id,
-									date_out: new Date(),
-									user_name: "Dave",
-								})
-								.then(() => res.sendStatus(200))
-								.catch((err: any) => {
-									console.log("Error: " + err);
-									res.sendStatus(400);
-								});
-						} else {
-							res.status(400).send("Copy Could Not Be Checked Out");
-						}
-					} else {
-						models.withdraw
-							.create({
-								copy_id: req.params.id,
-								date_out: new Date(),
-								user_name: "Dave",
-							})
-							.then(() => res.sendStatus(200))
-							.catch((err: any) => {
-								console.log("Error: " + err);
-								res.status(400).send("Copy Could Not Be Checked In");
-							});
-					}
-				} else {
-					res.status(400).json({ error: "Failed To Find Row", msg: "err" });
-				}
-			})
-			.catch((err: any) => {
-				res.status(400).send("Copy Not Found");
-			});
+		// 			const array = [...row.withdraws];
+
+		// 			array.sort((a, b) => {
+		// 				if (a.date_out < b.date_out) return 1;
+		// 				return -1;
+		// 			});
+
+		// 			if (array[0]) {
+		// 				if (array[0].date_in != null) {
+		// 					models.withdraw
+		// 						.create({
+		// 							copy_id: req.params.id,
+		// 							date_out: new Date(),
+		// 							user_name: "Dave",
+		// 						})
+		// 						.then(() => res.sendStatus(200))
+		// 						.catch((err: any) => {
+		// 							console.log("Error: " + err);
+		// 							res.sendStatus(400);
+		// 						});
+		// 				} else {
+		// 					res.status(400).send("Copy Could Not Be Checked Out");
+		// 				}
+		// 			} else {
+		// 				models.withdraw
+		// 					.create({
+		// 						copy_id: req.params.id,
+		// 						date_out: new Date(),
+		// 						user_name: "Dave",
+		// 					})
+		// 					.then(() => res.sendStatus(200))
+		// 					.catch((err: any) => {
+		// 						console.log("Error: " + err);
+		// 						res.status(400).send("Copy Could Not Be Checked In");
+		// 					});
+		// 			}
+		// 		} else {
+		// 			res.status(400).json({ error: "Failed To Find Row", msg: "err" });
+		// 		}
+		// 	})
+		// 	.catch((err: any) => {
+		// 		res.status(400).send("Copy Not Found");
+		// 	});
 	};
 
-	const checkCopyStatus = (req: Request, res: Response) => {
-		models.copy
-			.findByPk(parseInt(req.params.id), {
-				include: [{ model: models.withdraw, as: "withdraws" }],
-			})
-			.then((row: any) => {
-				if (row) {
-					if (row.withdraws.length === 0) {
-						res.json({ status: true });
-						return;
-					} else {
-						const withdraws = [...row.withdraws];
+	const checkCopyStatus = async (req: Request, res: Response) => {
 
-						withdraws.sort((a, b) => {
-							if (a.date_out < b.date_out) return 1;
-							return -1;
-						});
+		let id = parseInt(req.params.id);
+		res.json(await CopyService().CheckCopyStatus(id));
+		return;
 
-						if (withdraws[0].date_in === null) {
-							res.json({ status: false });
-						} else {
-							res.json({ status: true });
-						}
-					}
-				} else {
-					res.status(400).json({ error: "Failed To Find Copy" });
-				}
-			})
-			.catch((err: any) => {
-				res.status(400).json({ error: "Failed To Send Request" });
-			});
+
+		// models.copy
+		// 	.findByPk(parseInt(req.params.id), {
+		// 		include: [{ model: models.withdraw, as: "withdraws" }],
+		// 	})
+		// 	.then((row: any) => {
+		// 		if (row) {
+		// 			if (row.withdraws.length === 0) {
+		// 				res.json({ status: true });
+		// 				return;
+		// 			} else {
+		// 				const withdraws = [...row.withdraws];
+
+		// 				withdraws.sort((a, b) => {
+		// 					if (a.date_out < b.date_out) return 1;
+		// 					return -1;
+		// 				});
+
+		// 				if (withdraws[0].date_in === null) {
+		// 					res.json({ status: false });
+		// 				} else {
+		// 					res.json({ status: true });
+		// 				}
+		// 			}
+		// 		} else {
+		// 			res.status(400).json({ error: "Failed To Find Copy" });
+		// 		}
+		// 	})
+		// 	.catch((err: any) => {
+		// 		res.status(400).json({ error: "Failed To Send Request" });
+		// 	});
 	};
 
 	return {
--- notts-library-backend/src/service/copy-service.ts
+++ notts-library-backend/src/service/copy-service.ts
@@ -33,7 +33,97 @@ const CopyService = () => {
         return withdraws
     }
 
-    return { AddNewCopy, GetAllCopies, GetCopyByID, GetCopyWithdrawsByID }
+    const CheckinCopyByID = async (id: any): Promise<any> => {
+
+        const copy = await models.copy
+            .findByPk((id), {
+                include: [{ model: models.withdraw, as: "withdraws" }],
+            })
+
+        if (copy) {
+            //Can Copy Be Checked In?
+            const array = [...copy.withdraws];
+
+            array.sort((a, b) => {
+                if (a.date_out < b.date_out) return 1;
+                return -1;
+            });
+
+            if (array[0] && !array[0].date_in) {
+                models.withdraw.findByPk(array[0].id).then((rowb: any) => {
+                    rowb.update({ date_in: new Date() });
+                });
+            }
+        }
+    }
+
+    const CheckoutCopyByID = async (id: any): Promise<any> => {
+        const copy = await models.copy
+            .findByPk(id, {
+                include: [{ model: models.withdraw, as: "withdraws" }],
+            })
+
+
+        if (copy) {
+            //Can Copy Be Checked Out?
+
+            const array = [...copy.withdraws];
+
+            array.sort((a, b) => {
+                if (a.date_out < b.date_out) return 1;
+                return -1;
+            });
+
+            if (array[0]) {
+                if (array[0].date_in != null) {
+                    const withdraw = await models.withdraw
+                        .create({
+                            copy_id: id,
+                            date_out: new Date(),
+                            user_name: "Dave",
+                        })
+                }
+            } else {
+                models.withdraw
+                    .create({
+                        copy_id: id,
+                        date_out: new Date(),
+                        user_name: "Dave",
+                    })
+            }
+        }
+
+    }
+
+    const CheckCopyStatus = async (id: any): Promise<any> => {
+
+        const copy = await models.copy
+            .findByPk(id, {
+                include: [{ model: models.withdraw, as: "withdraws" }],
+            })
+
+        if (copy) {
+            if (copy.withdraws.length === 0) {
+                return { status: true };
+            } else {
+                const withdraws = [...copy.withdraws];
+
+                withdraws.sort((a, b) => {
+                    if (a.date_out < b.date_out) return 1;
+                    return -1;
+                });
+
+                if (withdraws[0].date_in === null) {
+                    return { status: false };
+                } else {
+                    return { status: true };
+                }
+            }
+        }
+        return copy;
+    }
+
+    return { AddNewCopy, GetAllCopies, GetCopyByID, GetCopyWithdrawsByID, CheckinCopyByID, CheckoutCopyByID, CheckCopyStatus }
 
 }
 
