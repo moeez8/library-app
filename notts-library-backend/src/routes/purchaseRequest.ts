7c6934cea5ea9cdbc9f650c5cc6e796f2a185ff7
moeez.shahid <moeez.shahid@bjss.com>
2022-06-15 10:24:44 +0100

    Updated purchase request service

--- notts-library-backend/src/routes/purchaseRequest.ts
+++ notts-library-backend/src/routes/purchaseRequest.ts
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const purchaseRequestAPI = require("../api/purchaseRequest-api");
 
-
-
 router.post("/", purchaseRequestAPI().addRequest);
 router.get("/", purchaseRequestAPI().searchForRequest);
 router.get("/:id", purchaseRequestAPI().getRequestByID);
