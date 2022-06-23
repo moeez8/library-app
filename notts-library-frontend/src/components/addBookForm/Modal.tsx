9621481af64f6fa68ab65702df2083eb484c9ad4
moeez.shahid <moeez.shahid@bjss.com>
2022-06-23 10:06:09 +0100

    Worked on modal for add book form to display full size image and description

--- notts-library-frontend/src/components/addBookForm/Modal.tsx
+++ notts-library-frontend/src/components/addBookForm/Modal.tsx
@@ -13,50 +13,60 @@ const Modal = (props: any) => {
         props.toggle();
     };
 
-    const [book, setbook] = useState<IBook>();
+    const truncate = (str: String) => {
+
+    }
+
+    const [book, setBook] = useState<IBook>();
 
     useEffect(() => {
         getBookData();
     }, []);
 
     const getBookData = async () => {
-        const bookData = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${props.bookIBAN}&jscmd=data&format=json`)
-        const data = await bookData.json();
 
-        if (bookData.status == 200 && Object.keys(data).length !== 0) {
-            setbook({
-                title: data[`ISBN:${props.bookIBAN}`].title,
-                //description: data[`ISBN:${props.bookIBAN}`].details.description,
-                author: data[`ISBN:${props.bookIBAN}`].authors[0].name,
-                imgURL: data[`ISBN:${props.bookIBAN}`].cover.medium
+        //Getting book data from Open Library 'details' api 
+        const bookDetails = await fetch(process.env.REACT_APP_BASE_URL + `/ol/${props.bookIBAN}`)
+        const details = await bookDetails.json();
+
+        if (bookDetails.status == 200 && Object.keys(details).length !== 0) {
+
+            //If book data is retrieved then get the 'works' data from the Open Library 'works' api using the 'works id'
+            const OLWorkSubDirectory: String = details[`ISBN:${props.bookIBAN}`].details.works[0].key
+            const OLWorkID = OLWorkSubDirectory.replaceAll('/', '').replaceAll('works', '');
+
+            const OLWork = await fetch(process.env.REACT_APP_BASE_URL + `/ol/works/${OLWorkID}`, {
+            })
+            const workData = await OLWork.json();
+            //console.log(workData.description)
+
+            setBook({
+                title: details[`ISBN:${props.bookIBAN}`].details.title,
+                description: workData.description.value || workData.description,
+                author: details[`ISBN:${props.bookIBAN}`].details.authors[0].name,
             });
         }
-
-        const openLibraryID = data[`ISBN:${props.bookIBAN}`].identifiers.openlibrary
-        const OLWork = await fetch(`https://openlibrary.org/works/${openLibraryID}.json`, {})
-        const workData = await OLWork.json();
-        console.log(workData)
 
     };
 
     if (book) {
 
         return (
-
             <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
                 <div className="flex justify-around">
-                    <div className="card mx-auto ">
+                    <div className="card mx-auto w-1/2">
                         <span className="float-right" onClick={handleClose}>&times;    </span>
-                        <h1 className="text-2xl font-bold m-1">Is this the book you're looking for?</h1>
+                        <h1 className="text-2xl font-bold m-1">Is this the book you're looking for?</h1> <br />
                         <div className="flex">
-                            <div className="m-1 break-words">
+                            <div className="flex-auto m-1 break-words">
                                 <h1 className="text-xl font-bold" > {book.title || "Undefined"}</h1 >
-                                <h1 className="text-lg">{book.author ? `Author: ${book.author}` : "Author: Undefined"}</h1>
-                                <h1 className="text-lg">{book.description ? `Description: ${book.description}` : "Description: No description found"}</h1>
-                                <h1 className="text-lg">{`IBAN: ${props.bookIBAN}`}</h1>
+                                <h1 className="text-lg">{book.author ? `Author: ${book.author}` : "Author: Undefined"}</h1><br />
+                                <h1 className="text-lg">{book.description ? `Description: ${book.description.split(" ").splice(0, 50).join(" ")}` : "Description: No description found"}</h1> <br />
+                                <h1 className="text-lg">{`ISBN: ${props.bookIBAN}`}</h1> <br />
                             </div >
-                            <div className="">
-                                <img src={book.imgURL} />
+                            <div className="flex-none self-center">
+
+                                <img src={`https://covers.openlibrary.org/b/isbn/${props.bookIBAN}-M.jpg`} />
                             </div>
                         </div>
 
